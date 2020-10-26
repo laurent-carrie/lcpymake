80d1ef3a7710f3dcc3caa52717089cd0287e54af
Laurent Carrié <laurent.carrie@gmail.com>
2020-10-26 21:06:43 +0100

    initial

--- lcpymake/a.py
+++ lcpymake/a.py
@@ -1,13 +1,19 @@
 from pathlib import Path
 from typing import List,Callable
 import subprocess
+import logging
+logging.basicConfig(level=logging.INFO)
 
 Rule = Callable[[ List[Path], List[Path]],subprocess.CompletedProcess]
 
-class TargetNotGeneratedException(BaseException):
+class TargetNotGeneratedException(Exception):
     def __init__(self,target:Path,process:subprocess.CompletedProcess):
         self.target = target
         self.process = process
+
+class SourceNotFoundException(Exception):
+    def __init__(self,source:Path):
+        self.source = source
 
 def f(i,j):
     return i+j
@@ -15,8 +21,9 @@ def f(i,j):
 
 def build_target(rule : Rule ,sources:List[Path],targets:List[Path]):
     for source in sources:
+        logging.debug(f"test source {source}")
         if not source.exists():
-            raise Exception(f"source does not exist : {source}")
+            raise SourceNotFoundException(source)
 
     p = rule(sources,targets)
 
@@ -24,6 +31,7 @@ def build_target(rule : Rule ,sources:List[Path],targets:List[Path]):
         raise Exception(f"rule return code : {p.returncode}, command was {p.args}")
 
     for target in targets:
+        logging.debug(f"test target {target}")
         if not target.exists():
             raise TargetNotGeneratedException(target,p)
 
--- test/test_1.py
+++ test/test_1.py
@@ -16,7 +16,7 @@ def wrong_make_obj(sources: List[Path], targets: [Path]) -> subprocess.Completed
 
     return p
 
-class Test_generate_o:
+class Test_1:
 
     def test_1(self,datadir):
         """
@@ -24,15 +24,23 @@ class Test_generate_o:
         """
 
         source = Path(datadir) / "hello.cpp"
-        target = Path("hello.o")
+        target = Path(datadir) / "hello.o"
         a.build_target(rule=a.make_obj,sources=[source],targets=[target])
 
+    def test_2(self,datadir) :
+        """
+        test that if a source does not exist, it will be caught
+        """
+        with pytest.raises(a.SourceNotFoundException):
+            source = Path(datadir) / "hello.x"
+            target = Path(datadir) / "hello.o"
+            a.build_target(rule=wrong_make_obj, sources=[source], targets=[target])
 
-    def test_2(self,datadir):
+    def test_3(self,datadir) :
         """
         test that if the file is not generated, it will be caught
         """
         with pytest.raises(a.TargetNotGeneratedException):
             source = Path(datadir) / "hello.cpp"
-            target = Path("hello.o")
+            target = Path(datadir) / "hello.o"
             a.build_target(rule=wrong_make_obj, sources=[source], targets=[target])
