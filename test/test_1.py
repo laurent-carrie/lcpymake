42702b98c240b090242c268045876cd4d96c9a85
Laurent Carrié <laurent.carrie@gmail.com>
2020-10-26 21:12:58 +0100

    initial

--- test/test_1.py
+++ test/test_1.py
@@ -8,39 +8,44 @@ import subprocess
 def func(x):
     return x + 1
 
-def wrong_make_obj(sources: List[Path], targets: [Path]) -> subprocess.CompletedProcess :
-    assert(len(sources)==1)
-    assert(len(targets)==1)
 
-    p = subprocess.run(['gcc', '-c','-o', str(targets[0].with_suffix(".x")),str(sources[0])])
+def wrong_make_obj(sources: List[Path], targets: [Path]) -> subprocess.CompletedProcess:
+    assert (len(sources) == 1)
+    assert (len(targets) == 1)
+
+    p = subprocess.run(
+        ['gcc', '-c', '-o', str(targets[0].with_suffix('.x')), str(sources[0])])
 
     return p
 
+
 class Test_1:
 
-    def test_1(self,datadir):
+    def test_1(self, datadir):
         """
         test that the .o file is correctly generated
         """
 
-        source = Path(datadir) / "hello.cpp"
-        target = Path(datadir) / "hello.o"
-        a.build_target(rule=a.make_obj,sources=[source],targets=[target])
+        source = Path(datadir) / 'hello.cpp'
+        target = Path(datadir) / 'hello.o'
+        a.build_target(rule=a.make_obj, sources=[source], targets=[target])
 
-    def test_2(self,datadir) :
+    def test_2(self, datadir):
         """
         test that if a source does not exist, it will be caught
         """
         with pytest.raises(a.SourceNotFoundException):
-            source = Path(datadir) / "hello.x"
-            target = Path(datadir) / "hello.o"
-            a.build_target(rule=wrong_make_obj, sources=[source], targets=[target])
+            source = Path(datadir) / 'hello.x'
+            target = Path(datadir) / 'hello.o'
+            a.build_target(rule=wrong_make_obj, sources=[
+                source], targets=[target])
 
-    def test_3(self,datadir) :
+    def test_3(self, datadir):
         """
         test that if the file is not generated, it will be caught
         """
         with pytest.raises(a.TargetNotGeneratedException):
-            source = Path(datadir) / "hello.cpp"
-            target = Path(datadir) / "hello.o"
-            a.build_target(rule=wrong_make_obj, sources=[source], targets=[target])
+            source = Path(datadir) / 'hello.cpp'
+            target = Path(datadir) / 'hello.o'
+            a.build_target(rule=wrong_make_obj, sources=[
+                source], targets=[target])
