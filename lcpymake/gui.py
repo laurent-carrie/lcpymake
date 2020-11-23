adc9c7a340808197079e2c0c6337d8f9ce609a20
Laurent Carrié <laurent.carrie@gmail.com>
2020-11-23 15:29:25 +0100

    remove snake

--- lcpymake/gui.py
+++ lcpymake/gui.py
@@ -51,34 +51,12 @@ def _main(screen, g: model.World):
     curses.curs_set(0)    # Hide the cursor
     screen.nodelay(True)  # Don't block I/O calls
 
-    directions = {
-        curses.KEY_UP: (-1, 0),
-        curses.KEY_DOWN: (1, 0),
-        curses.KEY_LEFT: (0, -1),
-        curses.KEY_RIGHT: (0, 1),
-    }
-
-    direction = directions[curses.KEY_RIGHT]
-    snake = [(0, i) for i in reversed(range(20))]
-
     while True:
         screen.erase()
         splash(screen)
 
         screen.addstr(1, 0, str(g.srcdir))
         screen.addstr(2, 0, str(g.sandbox), curses.color_pair(27))
-
-        # Draw the snake
-        screen.addstr(*snake[0], '@')
-        for segment in snake[1:]:
-            screen.addstr(*segment, '*')
-
-        # Move the snake
-        snake.pop()
-        snake.insert(0, tuple(map(sum, zip(snake[0], direction))))
-
-        # Change direction on arrow keystroke
-        direction = directions.get(screen.getch(), direction)
 
         screen.refresh()
         time.sleep(0.1)
